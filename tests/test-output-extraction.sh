134e8004102b034c83ad2754b540b4ef236ba562
Derek Misler <derek.misler@docker.com>
2026-06-11 19:08:24 +0000

    license: add Apache 2.0 headers and NOTICE file for open sourcing
    - Replace LICENSE file with verbatim Apache 2.0 text from apache.org
      (removes the appended copyright notice that was at line 179)
    - Add NOTICE file per Apache 2.0 guidelines
    - Add Apache 2.0 copyright header to all TypeScript source files in src/
    - Add Apache 2.0 copyright header to all YAML action files
    - Add Apache 2.0 copyright header to all GitHub workflow YAML files
    - Add Apache 2.0 copyright header to all shell scripts in scripts/ and tests/
    
    No generated files (dist/) or non-source files were modified.

--- tests/test-output-extraction.sh
+++ tests/test-output-extraction.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+
+# Copyright The Docker Agent Action Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Test output extraction logic from action.yml
 # Simulates the sanitize-output step's extraction methods
 
